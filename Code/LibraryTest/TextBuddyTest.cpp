fd082b36ee9860249ed83cb876baab8b4d00eaa7
petercd <petercdjasni@gmail.com>
2015-03-11 21:28:20 +0700

    second initial commit

---
+++ Code/LibraryTest/TextBuddyTest.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TextBuddyTest
+{		
+	TEST_CLASS(TextBuddyTest)
+	{
+	public:
+		
+		TEST_METHOD(SortTest)
+		{
+			TextBuddy textBuddy;
+
+			Assert::
+		}
+
+	};
+}
