285352d102bf7828a157bc0e79ee477562fb1b0b
Felix Fernando <a0114148@u.nus.edu>
2015-04-02 02:54:48 +0800

    Remove boost lib from task and taskmanager

--- src/Constan/Constan/TaskManager.h
+++ src/Constan/Constan/TaskManager.h
@@ -1,57 +1,34 @@
-#pragma once
-
 #ifndef TASKMANAGER_H_
 #define TASKMANAGER_H_
 
 #include "Task.h"
 #include <vector>
-#include <iterator>
-#include <algorithm>
 #include <fstream>
-#include <boost/serialization/vector.hpp>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <iostream>
 
 using namespace std;
-using namespace boost;
 
 static const string MATCH_FOUND = "The following entries match your query: ";
 
-struct CompareTaskName {
-   bool operator()(const Task& a, const Task& b) const {
-      return a.getTaskName() < b.getTaskName();
-   }
-};
-
-struct CompareTaskEndDate {
-   bool operator()(const Task& a, const Task& b) const {
-      return a.getEndDate() < b.getEndDate();
-   }
-};
-
-struct CompareTaskEndTime {
-   bool operator()(const Task& a, const Task& b) const {
-      return a.getEndTime() < b.getEndTime();
-   }
-};
-
 class TaskManager {
 private:
-	friend class boost::serialization::access;
-	template<class Archive>
-	void serialize(Archive & ar, const unsigned int version) {
-		ar & _tasks; 
-	}
-
-	static int _nextTaskID;
 	vector<Task> _tasks;
-	static TaskManager* _instance;
+	TaskManager* _instance;
 	TaskManager();
+	Task* _newTask;
+	int _taskID;
+	string _type;
 
 public:
-	static TaskManager* getInstance();
+	TaskManager* getInstance();
 	~TaskManager();
-	void append(Task task);
-	void addTask(Task task);
-	void deleteTask(vector<Task>& _tasks, int taskIDToDelete);
+	void readFromFile(vector<Task>& _tasks, char *argv[]);
+	void writeToFile(vector<Task>& _tasks, char *argv[]);
+	void addTask(string taskName, string startDate, string startTime, string endDate, string endTime, string type);
+	void deleteTask(int taskID);
 	vector<Task>* retrieveTimedTask(string timeIndicator);
 	void getTimedTaskVector(vector<Task>& timedTaskVector);
 	vector<Task>* retrieveFloatingTask(string timeIndicator);
@@ -59,14 +36,16 @@ public:
 	vector<Task>* retrieveDeadline(string timeIndicator);
 	void getDeadlineVector(vector<Task>& deadlineVector);
 	//void setCompletionStatus(int index, bool isComplete);
-	void editTask(int editIndex, string taskName, string startDate, string startTime, string endDate, string endTime);
+	void editTask(int editIndex, string taskName, string startTime, string endTime);
 	void clearAllTasks();
-	vector<Task> searchForString(string searchTerm);
-	vector<Task> searchForTasksDueToday(string todaysDate);
-	void sortTasksByName();
-	void sortTasksByNearestDeadline();
-	void save(const char* fileName);
-	void load(const char* fileName);
+	vector<Task>* searchForString(string keyword);
+	//void sortTasksByAscendingAlphabet(vector<Task> _tasks);
+	void sortTasksByNearestDeadline(vector<Task> _tasks);
+	void incrementTaskID();
+	string getTaskName(int taskID);
+	void editTask(int taskID, string taskName, string startDate, string startTime, string endDate, string endTime);
+	Task* findTask(int taskID);
+	string getType (string taskName, string startDate, string startTime, string endDate, string endTime);
 };
 
 #endif
