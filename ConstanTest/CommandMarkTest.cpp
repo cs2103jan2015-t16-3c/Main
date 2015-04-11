d47bc22b4f0b8a3e8fc7c974af9b6f6e9e0723ea
Felix Fernando <a0114148@u.nus.edu>
2015-04-12 00:50:31 +0800

    Implement testing for CommandAdd, CommandSearch, CommandEdit, CommandDelete, CommandMark and CommandUnmark

---
+++ ConstanTest/CommandMarkTest.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "CommandMark.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ConstanTest
+{
+	TEST_CLASS(CommandMarkTest)
+	{
+	public:
+		
+		TEST_METHOD(InverseCommandTest)
+		{
+			vector<Task>* _currentDisplay = new vector<Task>;
+			Command* cmd = new CommandMark (1, _currentDisplay);
+			Command* invCommand = cmd->getInverseCommand();
+			CommandUnmark* inv2 = dynamic_cast<CommandUnmark*>(invCommand);
+			Assert::IsNotNull(inv2);// assert that invCommand is of type CommandUnmark
+		}
+
+	};
+}
---
+++ ConstanTest/CommandUnmarkTest.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "CommandUnmark.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ConstanTest
+{
+	TEST_CLASS(CommandUnmarkTest)
+	{
+	public:
+		
+		TEST_METHOD(InverseCommandTest)
+		{
+			vector<Task>* _currentDisplay = new vector<Task>;
+			Command* cmd = new CommandUnmark (1, _currentDisplay);
+			Command* invCommand = cmd->getInverseCommand();
+			CommandMark* inv2 = dynamic_cast<CommandMark*>(invCommand);
+			Assert::IsNotNull(inv2);// assert that invCommand is of type CommandMark
+		}
+
+	};
+}
