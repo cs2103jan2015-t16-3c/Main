dbefa220aaf2696b0e86e671d735a52399b4bc9e
Felix Fernando <a0114148@u.nus.edu>
2015-04-12 16:19:12 +0800

    Test naming

--- ConstanTest/CommandMarkTest.cpp
+++ ConstanTest/CommandMarkTest.cpp
@@ -10,7 +10,7 @@ namespace ConstanTest
 	{
 	public:
 		
-		TEST_METHOD(InverseCommandTest)
+		TEST_METHOD(InverseCommandMark)
 		{
 			vector<Task>* _currentDisplay = new vector<Task>;
 			Command* cmd = new CommandMark (1, _currentDisplay);
---
+++ ConstanTest/LogicTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Logic.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ConstanTest
+{
+	TEST_CLASS(LogicTest)
+	{
+	public:
+		
+		TEST_METHOD(ProcessCommand)
+		{
+			Logic logic;
+		}
+
+	};
+}
